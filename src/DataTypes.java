94624213c2d3b12d122f7d0e735fbe2c7170c7be
Nihad Əzizli <nihad.azizli.12@gmail.com>
2025-10-14 00:23:43 +0400

    123

---
+++ src/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age=25;
+        double price=19.99;
+        char a='A';
+        boolean isStudent=true;
+        System.out.println(age+ " " + price+ " " + a+ " " + isStudent);
+
+        double num=12.99;
+        int numNum=(int) num;
+        System.out.println(num+ " "+ numNum);
+
+        boolean isAdualt = true,hasDrivingLicense = false;
+        System.out.println(isAdualt+" "+hasDrivingLicense);
+
+
+        String myFullName = "Nihat Azizli";
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,15 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a=15,b=4;
+        System.out.println((a+b)+" "+(a-b)+" "+(a * b)+" "+(a / b)+" a"+(a % b));
+
+        double x=7.5;
+        float y=2.0f;
+        System.out.println(x/y);
+
+        int d = 10 ,e = 5 ,c = 10;
+
+        System.out.println((d>e)+" "+(d<e)+" "+(d==c)+" "+(d!=e)+" "+(e>=d));
+
+    }
+}
